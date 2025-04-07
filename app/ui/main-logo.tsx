63e636bfea0a5e44acfe5254ebb03c5483c9e062
Marl Bermudo <marlberm2014@gmail.com>
2025-04-07 19:28:39 +0800

    [Sample_Code14] Improve Accessibility 1. Change logo to simple style and added without background 2. Change some UI on the main logo 3. Added form validation and accessibility support

--- app/ui/main-logo.tsx
+++ app/ui/main-logo.tsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 export default function MainLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center shrink-1 justify-center leading-none text-white md:justify-start md:shrink-0`}
+      className={`${lusitana.className} flex flex-row items-center shrink-1 justify-center leading-none text-black md:justify-start md:shrink-0 group-hover:text-white`}
     >
       <Image
         className="relative dark:drop-shadow-[0_0_0.1px_#ffffff70]"
-        src="/android-chrome-192x192.png"
+        src="/android-chrome-wo-bg-192x192.png"
         alt="Marl Logo"
         width={60}
         height={48}
         priority
       />
-      <p className="text-[32px]">Marl Bermudo</p>
+      <p className="text-[28px]">Marl Bermudo</p>
     </div>
   );
 }
